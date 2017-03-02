d521cfa446ec722dee575a2c6e26d961f7ca5461
leoybkim <leoybkim@gmail.com>
2017-03-02 02:01:26 -0500

    Create basic todo list features

--- app/src/main/java/com/leoybkim/justdoit/MainActivity.java
+++ app/src/main/java/com/leoybkim/justdoit/MainActivity.java
@@ -1,13 +1,35 @@
 package com.leoybkim.justdoit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItem = (ListView) findViewById(R.id.listView);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItem.setAdapter(itemsAdapter);
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.editText);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
     }
 }
